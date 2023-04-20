e9aa1ba90b0f17bcc45b85b91aefe3e16d1a2726
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-04-21 04:35:55 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Cards from "./components/card";
+import "./style/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
